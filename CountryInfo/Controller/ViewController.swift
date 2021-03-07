b508155d4247396197893b38c9f4dab6e7ce22a3
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-03-07 20:15:29 -0300

    ATT organização de arquivos

--- CountryInfo/Controller/ViewController.swift
+++ CountryInfo/Controller/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func loadCountriesFromJson() {
+        let decoder = JSONDecoder()
+        // parse JSON
+    }
 }
 
---
+++ CountryInfo/Model/Country.swift
@@ -0,0 +1,14 @@
+//
+//  Country.swift
+//  CountryInfo
+//
+//  Created by Joao Gabriel Dourado Cervo on 07/03/21.
+//
+
+import Foundation
+
+struct Country: Decodable {
+    var name: String
+    var capital: String
+    var population: String
+}
