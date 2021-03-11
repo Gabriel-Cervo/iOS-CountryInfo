c02a77137b14978e5e9480f144634439c1343307
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-03-11 15:55:50 -0300

    ADD leitura do JSON com os paises

--- CountryInfo/Controller/ViewController.swift
+++ CountryInfo/Controller/ViewController.swift
@@ -8,13 +8,31 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var countries: [Country] = [Country]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loadCountriesFromJson()
     }
     
     func loadCountriesFromJson() {
         let decoder = JSONDecoder()
-        // parse JSON
+        
+        guard let url = Bundle.main.url(forResource: "countries", withExtension: "json") else {
+            print("Can't find countries json!")
+            return
+        }
+        
+        do {
+            let jsonData = try Data(contentsOf: url)
+            let decodedInfo = try decoder.decode([Country].self, from: jsonData)
+        
+            countries = decodedInfo
+        } catch {
+            print("Error while deconding data:")
+            print(error)
+        }
     }
 }
 
